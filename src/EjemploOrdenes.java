d264c8d413701c30d24e67f5cdb9ce73ddcda765
Eduardo Ramos <eduardoramosrosales16@gmail.com>
2024-06-16 03:32:41 -0600

    Se crea elementos de tipo privado y se inicializa autoincremento de id y se crea arreglo productos de 4 elementos

--- src/EjemploOrdenes.java
+++ src/EjemploOrdenes.java
@@ -1,5 +1,5 @@
 public class EjemploOrdenes {
     public static void main(String[] args) {
-        
+
     }
 }
--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -1,2 +1,16 @@
+import java.util.Date;
+
 public class OrdenCompra {
+    private int id;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos;
+
+    private static int ultimoId;
+
+    public OrdenCompra(){
+        this.id = ++ultimoId;
+        this.productos = new Producto[4];
+    }
 }
