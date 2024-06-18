7fd694354a26ee14a76f1fc4d023b3f9a399f943
Eduardo Ramos <eduardoramosrosales16@gmail.com>
2024-06-18 02:41:51 -0600

    Se crearon 3 compras con sus respectivos clientes y productos

--- src/EjemploOrdenes.java
+++ src/EjemploOrdenes.java
@@ -1,5 +1,16 @@
 public class EjemploOrdenes {
     public static void main(String[] args) {
+        OrdenCompra compra1 = new OrdenCompra();
+        OrdenCompra compra2 = new OrdenCompra();
+        OrdenCompra compra3 = new OrdenCompra();
 
+        compra1.setCliente(new Cliente("Charles", "Ramos"));
+        compra1.addProducto(new Producto("Sabritas", "Papas", 15));
+
+        compra2.setCliente(new Cliente("Eduardo", "Ramos"));
+        compra2.addProducto(new Producto("Coca cola", "Refresco", 35));
+
+        compra3.setCliente(new Cliente("Naomi", "Gonzalez"));
+        compra3.addProducto(new Producto("Turin", "Conejos turin", 247));
     }
 }
